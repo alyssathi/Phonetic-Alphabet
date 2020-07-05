a69478caa6ade7c6b0f542febaebedc2af8a32ec
AlyssaThi <aruefenacht14@gmail.com>
2020-07-05 20:43:03 +1000

    refactored switch into obj

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 const input = document.getElementById("input");
 const submit = document.getElementById("submit");
 
+const legend = {
+    "A": "Alpha",
+    "B": "Bravo",
+    "C": "Charlie",
+    "D": "Delta",
+    "E": "Echo",
+    "F": "Foxtrot",
+    "G": "Golf",
+    "H": "Hotel",
+    "I": "India",
+    "J": "Juliet",
+    "K": "Kilo",
+    "L": "Lima",
+    "M": "Mike",
+    "N": "November",
+    "O": "Oscar",
+    "P": "Papa",
+    "Q": "Quebec",
+    "R": "Romeo",
+    "S": "Sierra",
+    "T": "Tango",
+    "U": "Uniform",
+    "V": "Victor",
+    "W": "Whiskey",
+    "X": "X-ray",
+    "Y": "Yankee",
+    "Z": "Zulu"
+};
 
 
 const inputFunc = () => {
@@ -8,90 +36,9 @@ const inputFunc = () => {
         let inputArr = input.value.split("");
         let outputArr = [];
         inputArr.map((letter) => {
-            switch (true) {
-                case letter === "a" || letter ==="A":
-                   outputArr.push(`${letter} as in 'Alpha'`);
-                break;
-                case letter === "b" || letter === "B":
-                    outputArr.push(`${letter} as in 'Bravo'`);
-                break;
-                case letter === "c" || letter === "C":
-                    outputArr.push(`${letter} as in 'Charlie'`);
-                break;
-                case letter === "d" || letter === "D":
-                    outputArr.push(`${letter} as in 'Delta'`);
-                break;
-                case letter === "e" || letter === "E":
-                    outputArr.push(`${letter} as in 'Echo'`);
-                break;
-                case letter === "f" || letter === "F":
-                    outputArr.push(`${letter} as in 'Foxtrot'`);
-                break;
-                case letter === "g" || letter === "G":
-                    outputArr.push(`${letter} as in 'Golf'`);
-                break;
-                case letter === "h" || letter === "H":
-                    outputArr.push(`${letter} as in 'Hotel'`);
-                break;
-                case letter === "i" || letter === "I":
-                    outputArr.push(`${letter} as in 'India'`);
-                break;
-                case letter === "j" || letter === "J":
-                    outputArr.push(`${letter} as in 'Juliet'`);
-                break;
-                case letter === "k" || letter === "K":
-                    outputArr.push(`${letter} as in 'Kilo'`);
-                break;
-                case letter === "l" || letter === "L":
-                    outputArr.push(`${letter} as in 'Lima'`);
-                break;
-                case letter === "m" || letter === "M":
-                    outputArr.push(`${letter} as in 'Mike'`);
-                break;
-                case letter === "N" || letter === "n":
-                    outputArr.push(`${letter} as in 'November'`);
-                break;
-                case letter === "o" || letter === "O":
-                    outputArr.push(`${letter} as in 'Oscar'`);
-                break;
-                case letter === "p" || letter === "P":
-                    outputArr.push(`${letter} as in 'Papa'`);
-                break;
-                case letter === "q" || letter === "Q":
-                    outputArr.push(`${letter} as in 'Quebac'`);
-                break;
-                case letter === "r" || letter === "R":
-                    outputArr.push(`${letter} as in 'Romeo'`);
-                break;
-                case letter === "s" || letter === "S":
-                    outputArr.push(`${letter} as in 'Sierra'`);
-                break;
-                case letter === "t" || letter === "T":
-                    outputArr.push(`${letter} as in 'Tango'`);
-                break;
-                case letter === "u" || letter === "U":
-                    outputArr.push(`${letter} as in 'Uniform'`);
-                break;
-                case letter === "v" || letter === "V":
-                    outputArr.push(`${letter} as in 'Victor'`);
-                break;
-                case letter === "w" || letter === "W":
-                    outputArr.push(`${letter} as in 'Whiskey'`);
-                break;
-                case letter === "x" || letter === "X":
-                    outputArr.push(`${letter} as in 'X-ray'`);
-                break;
-                case letter === "y" || letter === "Y":
-                    outputArr.push(`${letter} as in 'Yankee'`);
-                break;
-                case letter === "z" || letter === "Z":
-                    outputArr.push(`${letter} as in 'Zulu'`);
-                break;
-
-                default:
-                    outputArr.push(`${letter}`);
-                break;
-            }})
+            let cap = letter.toUpperCase();
+            legend.hasOwnProperty(cap) ? outputArr.push(`${cap} as in '${legend[cap]}'`) : outputArr.push(`${cap}`);
+        })
         input.value = "";
         document.getElementById("output").innerHTML = outputArr.join("<br>");
     }
